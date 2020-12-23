3aef932e8b0857ad685d1ba03c4e6d9e2e82ba56
amagnosouza <alexandremagnofs@gmail.com>
2020-12-22 23:44:28 -0300

    blocos de codigo

---
+++ inicio/desafio_blocos.js
@@ -0,0 +1,18 @@
+/*
+    Exibir no console os numeros pares e impares de 1 a 10 em blocos separados
+*/
+console.log("Impar")
+{
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+}
+console.log("Par")
+{
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+    console.log(10)
+}
