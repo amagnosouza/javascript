cc61e3fc28f7a18a4443dba6da7dc0b18a1e276f
Alexandre Souza <alexandremagnofs@gmail.com>
2020-12-23 16:44:23 -0300

    dados

---
+++ inicio/dados_1.js
@@ -0,0 +1,18 @@
+// variaveis
+
+var nome = "Caneta" // string
+var quantidade = 10 // inteiros
+var preco = 6.4 // float
+let imposto = 1.5 // => nova fomra de declarar varivel
+let precoFinal = preco + imposto
+
+outroVar = "outroVar"
+console.log(nome)
+console.log(quantidade)
+console.log(preco)
+console.log(imposto)
+console.log(precoFinal)
+
+nome = "Caneta Azul! Azul Caneta!"
+console.log(nome)
+console.log(outroVar)
---
+++ inicio/dados_2.js
@@ -0,0 +1,8 @@
+let preco = 20.90
+let desconto = 0.4
+let precoComDesconto = preco * (1 - desconto)
+
+console.log(20.9 * 0.6)
+console.log(preco * (1 - desconto))
+console.log(precoComDesconto)
+
