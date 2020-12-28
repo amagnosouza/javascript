2d3eef9a3bcfcf5e58af8bf848703517967bb201
Alexandre Souza <alexandremagnofs@gmail.com>
2020-12-28 09:38:19 -0300

    dados_2

--- inicio/dados_2.js
+++ inicio/dados_2.js
@@ -2,7 +2,14 @@ let preco = 20.90
 let desconto = 0.4
 let precoComDesconto = preco * (1 - desconto)
 
-console.log(20.9 * 0.6)
-console.log(preco * (1 - desconto))
-console.log(precoComDesconto)
+console.log(20.9 * 0.6) // valor com desconto
+console.log(preco * (1 - desconto)) 
+console.log(precoComDesconto) // variavel => primeira letra minusculo e cada nova palavra com maisculo
 
+let nome = "Livro"
+let categoria = "Livraria"
+console.log("Produto: " + nome 
+    + ", Categoria: " + categoria 
+    + ", Preço: " + preco 
+    + ", Desconto: " + desconto 
+    + "Valor Total: " + precoComDesconto) // concatenar string
