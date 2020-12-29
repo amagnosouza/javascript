4340fb6715c9393acfb9b064a0a5e0e26683c6cd
Alexandre Souza <alexandremagnofs@gmail.com>
2020-12-29 15:35:19 -0300

    Operadores atribuicoes

---
+++ operadores/atribuicao.js
@@ -0,0 +1,20 @@
+console.log("-------------------------------------------------------------")
+
+let x = 10
+x += 6 //x = x + 6 => atribuição de soma
+console.log(x)
+x -= 6 //x = x - 6 => atribuição de subtracao
+console.log(x)
+x *= 6 //x = x + 6 => atribuição de multiplicacao
+console.log(x)
+x /= 6 //x = x / 6 => atribuição de divisao
+console.log(x)
+x %= 6 //x = x % 6 => atribuição de modulo
+console.log(x)
+
+console.log("-------------------------------------------------------------")
+
+let palavra = "Texto testado"
+let jmp = ", eu não sou testado"
+jmp += "agora foi e voltou"
+console.log(palavra + jmp)
---
+++ operadores/desafio_atribuicao.js
@@ -0,0 +1,19 @@
+// let texto = "Bom dia!\nTudo bem ?" // quebra de linha
+// console.log(texto)
+
+/* 
+1. Ana Silva
+2. Pedro Albuquerque
+3. Guilheme Pereira
+4. Rebeca França
+*/
+
+
+let texto = "Lista de APROVADOS"
+texto += "\n==================\n"
+texto += "\n1. Ana Silva"
+texto += "\n2. Pedro Albuquerque"
+texto += "\n3. Guilheme Pereira"
+texto += "\n4. Rebeca França"
+
+console.log(texto)
