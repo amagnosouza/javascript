d7f45c299b71a0ca5396ccd3844dd80b8b750dfd
Alexandre Souza <alexandremagnofs@gmail.com>
2020-12-29 11:05:41 -0300

    Operadores relacionais

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,14 @@
+let a = 1
+let b = 2
+//  encontre 5 expressões que o resultado é TRUE
+console.log(a != b)
+console.log(b > a)
+console.log(a == (b - 1))
+console.log(b != (a - 1))
+console.log((a + a) == b)
+//  encontre 5 expressões que o resultado é FALSE
+console.log(b < a)
+console.log(((a + a) / 2 > b))
+console.log(a + b == 5)
+console.log(a == a && a > b)
+console.log(b < a || a > b)
---
+++ operadores/relacionais_1.js
@@ -0,0 +1,17 @@
+// valores literais
+console.log(7 > 9)
+console.log(7 >= 7)
+console.log(8 < 2)
+console.log(8 <= 12)
+console.log(9 == 3)
+console.log(9 != 5)
+
+let a = 7
+let b = 32
+
+console.log(a > b)  // maior
+console.log(a >= b) // maior ou igual
+console.log(a < b)  // menor
+console.log(a <= b) // menot ou igual
+console.log(a == b) // igual
+console.log(a != b) // diferente
