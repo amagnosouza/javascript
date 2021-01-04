c562cc04e13516944d8c4659ed1f4d80b654dcfa
Alexandre Souza <alexandremagnofs@gmail.com>
2021-01-04 14:39:43 -0300

    Operadores ternarios

---
+++ operadores/ternario_1.js
@@ -0,0 +1,4 @@
+const hora = 15
+//estrutura do operador ternario => Variavel = exp ? se true : se false
+const saudacao = hora >= 10? "Boa tarde": "Boa noite" 
+console.log(saudacao)
---
+++ operadores/ternario_2.js
@@ -0,0 +1,4 @@
+const hora = 23
+// com mais de 2 operadores
+const saudacao = hora <= 11 ? "Bom dia" : hora <= 17 ? "Boa tarde" : "Boa noite"
+console.log(saudacao)
