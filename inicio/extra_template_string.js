20e80d80e6e63e6bcd1a814a02dd76eb374d06da
Alexandre Souza <alexandremagnofs@gmail.com>
2021-01-07 14:02:26 -0300

    formatar string

---
+++ inicio/extra_template_string.js
@@ -0,0 +1,11 @@
+const i = 5
+const j = 28
+
+console.log("I = " + i + ", J = " + j)
+console.log('I = ' + i + ', J = ' + j)
+
+console.log(`I = ${i}, J = ${j}`) //template string
+console.log(`${Math.random()} => Valor aleatorio`)
+console.log(`1 + 2 = ${1 + 2} e esta =>\né uma quebra de linha`)
+
+
